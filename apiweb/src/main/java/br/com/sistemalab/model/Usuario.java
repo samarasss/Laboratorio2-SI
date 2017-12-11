07f2f2bfd9f085a7c7a56c4a31d819968870b5b9
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2017-12-11 09:45:23 -0300

    Problemas para acessar o banco

--- apiweb/src/main/java/br/com/sistemalab/model/Usuario.java
+++ apiweb/src/main/java/br/com/sistemalab/model/Usuario.java
@@ -1,8 +1,11 @@
 package br.com.sistemalab.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
+@Entity
 public class Usuario {
 	
 	@Id
